b73050274f9973688f978b3f5983497c6ed837e1
Matt Ryan <matthewryan716@gmail.com>
2024-12-29 20:46:23 -0500

    Setting up NativeWind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,6 @@ import { Text } from "react-native";
 export default function HomeScreen() {
 
     return (
-        <Text>Home</Text>
+        <Text className="font-bold text-4xl p-10 bg-red-500 color-blue-500">Home screen</Text>
     )
 }
